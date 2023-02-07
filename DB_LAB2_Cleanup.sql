a84e8314b1a6f15ecd55409a5604bdb2d2101578
Jacob Krucinski <jacob1576@gmail.com>
2023-02-07 20:42:08 +0100

    FIXED THE SQL BUG. Turns out to have been a column misordering in the Movie CSV file

--- DB_LAB2_Cleanup.sql
+++ DB_LAB2_Cleanup.sql
@@ -1,12 +1,14 @@
--- DB_Lab2_Cleanup.sql
--- Written by Jacob Krucinski on 04/02/23
+-- DB_Lab2_Cleanup_v2.sql
+-- Written by Jacob Krucinski on 07/02/23
 --
--- Cleanup of original database setup file
+-- Attempt 2 for cleaning up database with new structure
 --
 -- TODO:
 -- ** Rename to lab2.sql later
 -- Consider switching variables to snake case (as seen in lectures)
 -- Consider changing table names to plural (as seen in lecture notes)
+--
+-- ** NOTE: Constraints below will fail if there is no data in the other tables (referencing a NULL)
 
 -- Init
 PRAGMA foreign_keys = OFF;
@@ -30,52 +32,52 @@ CREATE TABLE Theater
 
 CREATE TABLE Performance
 (
+    PerformanceId   INT,
     StartTime       TIME,
     PerformanceDate DATE,
     TheaterName     VARCHAR(50) NOT NULL,
     MovieTitle      VARCHAR(50) NOT NULL,
+    IMDBKey         CHAR(9),
 
-    CONSTRAINT      PK_DateTimeName             PRIMARY KEY(StartTime, PerformanceDate, TheaterName)
-    -- ** NOTE: Constraints below will fail if there is no data in the other tables (referencing a NULL)
+    CONSTRAINT      PK_DateTimeName                     PRIMARY KEY(PerformanceId)
 
-    -- CONSTRAINT      FK_Performance_Theater_TheaterName   FOREIGN KEY(TheaterName)
-    -- REFERENCES      Theater(TheaterName)
-    CONSTRAINT      FK_Performance_Movie_MovieTitle      FOREIGN KEY(MovieTitle)
-    REFERENCES      Movie(MovieTitle)
-    -- CONSTRAINT      FK_Performance_Ticket_TicketId       FOREIGN KEY(TicketId)
-    -- REFERENCES      Ticket(TicketId)
+    CONSTRAINT      FK_Performance_Theater_TheaterName  FOREIGN KEY(TheaterName)
+    REFERENCES      Theater(TheaterName)
+
+    CONSTRAINT      FK_Performance_Movie_IMDBKey        FOREIGN KEY(IMDBKey)
+    REFERENCES      Movie(IMDBKey)
 );
 
 CREATE TABLE Movie
 (
+    IMDBKey         CHAR(9) NOT NULL,
     MovieTitle      VARCHAR(50) NOT NULL,
     ProductionYear  INT,
-    IMDBKey         CHAR(9),
     RunningTime     INT,
 
-    CONSTRAINT      PK_Movie_MovieTitle         PRIMARY KEY(MovieTitle)
+    CONSTRAINT      PK_Movie_MovieTitle                 PRIMARY KEY(IMDBKey)
 );
 
 CREATE TABLE Ticket
 (
     TicketId        TEXT DEFAULT (lower(hex(randomblob(16)))),
+    PerformanceId   INT,
     Username        VARCHAR(50) NOT NULL,
-    StartTime       TIME,
-    PerformanceDate DATE,
-    TheaterName     VARCHAR(50) NOT NULL,
+
+    CONSTRAINT      PK_TicketId                 PRIMARY KEY(TicketId)
+
+    CONSTRAINT      FK_Performance              FOREIGN KEY(PerformanceId)
+    REFERENCES      Performance(PerformanceId)
 
     CONSTRAINT      FK_Ticket_Costumer_Username FOREIGN KEY(Username)
     REFERENCES      Customer(Username)
-    -- Need to define foreign keys in tuples IF defined as tuples for primary key in another table
-    CONSTRAINT      FK_Performance              FOREIGN KEY(StartTime, PerformanceDate, TheaterName)
-    REFERENCES      Performance(StartTime, PerformanceDate, TheaterName)
 );
 
 CREATE TABLE Customer
 (
-    Username        VARCHAR(50),
-    CustomerName    VARCHAR(50),
-    UserPassword    VARCHAR(50),
+    Username        VARCHAR(50) NOT NULL,
+    CustomerName    VARCHAR(50) NOT NULL,
+    UserPassword    VARCHAR(50) NOT NULL,
 
     CONSTRAINT      PK_Costumer_Username        PRIMARY KEY(Username)
 );
@@ -103,34 +105,51 @@ BEGIN TRANSACTION;
 
 -- .mode csv Performance
 -- .import SampleData_Performance.csv Performance
-
 INSERT OR   REPLACE
-INTO        Performance(StartTime, PerformanceDate, TheaterName, MovieTitle)
-VALUES      ("10:00","2023-01-15","Artcraft Theater","Avatar: The Way of Water"),
-            ("13:15","2023-01-01","Artcraft Theater","Forrest Gump"),
-            ("17:00","2023-01-23","Artcraft Theater","Forrest Gump"),
-            ("21:20","2023-01-11","Booth Theater","Silence of the Lambs"),
-            ("19:10","2022-10-06","Capitol Theater","The Dark Night"),
-            ("22:45","2022-10-06","Capitol Theater","The Dark Night"),
-            ("19:35","2022-12-16","Cliff Theater","The Dark Night"),
-            ("14:00","2022-12-17","Los Angeles Theater","The Breakfast Club"),
-            ("18:30","2022-12-18","Los Angeles Theater","The Breakfast Club"),
-            ("18:15","2022-12-19","Paramount Theater","Top Gun"),
-            ("21:20","2022-12-19","Paramount Theater","Top Gun"),
-            ("18:15","2022-12-19","Tower Theater","Top Gun"),
-            ("23:00","2023-02-01","Vista Theater","Top Gun"),
-            ("09:20","2023-02-04","Garneau Theater","Ponyo"),
-            ("19:25","2023-02-04","Royal Cinema","Ponyo"),
-            ("12:30","2049-01-01","Atlantic","Blade Runner 2049"),
-            ("21:00","2022-11-06","Astoria","The Terminator"),
-            ("22:20","2022-11-06","Maximteatern","The Terminator"),
-            ("13:30","2022-08-04","Draken","Mission Impossible - Fallout");
+INTO        Performance(PerformanceId, StartTime, PerformanceDate, TheaterName, MovieTitle, IMDBKey)
+VALUES      (1,"10:00","2023-01-15","Artcraft Theater","Avatar: The Way of Water","tt1630029"),
+            (2,"13:15","2023-01-01","Artcraft Theater","Forrest Gump","tt0109830"),
+            (3,"17:00","2023-01-23","Artcraft Theater","Forrest Gump","tt0109830"),
+            (4,"21:20","2023-01-11","Booth Theater","Silence of the Lambs","tt0102926"),
+            (5,"19:10","2022-10-06","Capitol Theater","The Dark Night","tt0468569"),
+            (6,"22:45","2022-10-06","Capitol Theater","The Dark Night","tt0468569"),
+            (7,"19:35","2022-12-16","Cliff Theater","The Dark Night","tt0468569"),
+            (8,"14:00","2022-12-17","Los Angeles Theater","The Breakfast Club","tt0088847"),
+            (9,"18:30","2022-12-18","Los Angeles Theater","The Breakfast Club","tt0088847"),
+            (10,"18:15","2022-12-19","Paramount Theater","Top Gun","tt0092099"),
+            (11,"21:20","2022-12-19","Paramount Theater","Top Gun","tt0092099"),
+            (12,"18:15","2022-12-19","Tower Theater","Top Gun","tt0092099"),
+            (13,"23:00","2023-02-01","Vista Theater","Top Gun","tt0092099"),
+            (14,"09:20","2023-02-04","Garneau Theater","Ponyo","tt0876563"),
+            (15,"19:25","2023-02-04","Royal Cinema","Ponyo","tt0876563"),
+            (16,"12:30","2049-01-01[","Atlantic","Blade Runner 2049","tt1856101"),
+            (17,"21:00","2022-11-06","Astoria","The Terminator","tt0088247"),
+            (18,"22:20","2022-11-06","Maximteatern","The Terminator","tt0088247"),
+            (19,"13:30","2022-08-04","Draken","Mission Impossible - Fallout","tt4912910");
+
+-- Now fill in performance IDs
+INSERT OR   REPLACE
+INTO        Ticket(Username, PerformanceId)
+VALUES      ("test123",1),
+            ("test123",2),
+            ("test123",3),
+            ("jacob1576",3),
+            ("jacob1576",3),
+            ("alice2002",4),
+            ("jacob1576",5),
+            ("brian22",19),
+            ("test123",12),
+            ("test123",13),
+            ("jacob1576",14),
+            ("jacob1576",17),
+            ("alice2002",15),
+            ("jacob1576",16),
+            ("brian22",18);
 
 -- REWRITE TO ADD MANUALLY (so that default ticket UUIDs are used)
-.mode csv Ticket
-.import SampleData_Ticket.csv Ticket
 
 END TRANSACTION;
--- PRAGMA foreign_keys = ON;
+
+--PRAGMA foreign_keys = ON;
 
 -- NOTE: Replace the import commands above with real INSERT commands later
