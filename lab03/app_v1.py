dad2800115dc02e0e35dcdcc43a94e4db0279b82
Jacob Krucinski <jacob1576@gmail.com>
2023-02-14 14:22:00 +0100

    Created skeleton for REST server

--- lab03/app_v1.py
+++ lab03/app_v1.py
@@ -23,11 +23,93 @@ db      = sqlite3.connect("theaters.sqlite")
 ####### Main REST server functions ########
 @get('/ping')
 def get_ping_pong():
+    """
+    Return string 'pong' when <host>:<port>/ping is requested
+    """
     # Set 200 - OK status
     request.status = 200
 
     # Return pong string
     return "pong"
 
-# Start running REST server
+@post('/reset')
+def reset_db():
+    """
+    Reset the database (remove all entries)
+    TODO: Jacob
+
+    Should we do this using transactions??
+    """
+    pass
+
+@post('/users')
+def add_customer():
+    """
+    Add a user to the Customer table (if they don't already exist)
+
+    Raise errors if this happens
+    """
+    customer_data = request.json
+    pass
+
+@post('/movies')
+def add_movie():
+    """
+    Add a movie to the Movie table
+
+    Raise errors if this happens
+    """
+    movie_data = request.json
+    pass
+
+@get('/movies')
+def get_movies():
+    """
+    Get all movies from the Movie table
+    Make sure to edit query based on query strings (e.g. ?name=asdf)
+
+    Returned in JSON format
+    """
+    pass
+
+@get('/movies/<imdb_key>')
+def get_specific_movie(imdb_key):
+    """
+    Get a specific movie from the Movie table based on the IMDB key
+    """
+    pass
+
+@post('/performances')
+def add_performance():
+    """
+    Add a performance to the Performance table
+
+    Raise errors if this happens
+    """
+    performance_data = request.json
+    pass
+
+@get('/performances')
+def get_performances():
+    """
+    Get all performances from the Performance table
+    """
+    pass
+
+@post('/tickets')
+def add_ticket():
+    """
+    Add a ticket to the Ticket table
+    """
+    pass
+
+@get('/users/<username>/tickets')
+def get_users_ticket():
+    """
+    Get all ticket information for a specific user
+    """
+    pass
+
+
+##### Start running REST server #####
 run(host=HOST, port=PORT)
